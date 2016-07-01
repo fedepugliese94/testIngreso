5b7eb61bf05e4c2dadc1189269637e4e8c08815f
fedepugliese94 <fedepugliese94@gmail.com>
2016-07-01 20:34:02 -0300

    Aca arme el ejercicio 8

--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -4,5 +4,14 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	alert("resto");
+	var numeroUno;
+	var numeroDos;
+
+	numeroUno = document.getElementById('numeroDividendo').value;
+	numeroDos = document.getElementById('numeroDivisor').value;
+
+	alert("El resto es " + numeroUno % numeroDos);
+
+
+
 }
